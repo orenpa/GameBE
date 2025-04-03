5f6bc4257c42e5fb0e74ab0d1ed8b28f8c4ec883
Oren Partovi <orenp@moveo.co.il>
2025-04-03 14:39:48 +0300

    Add logType field to log model and update player service for logging actions
    - Introduced logType field in log model to categorize logs as 'info', 'error', 'crash', or 'critical'.
    - Enhanced PlayerService to publish logs for player creation, retrieval, update, and deletion actions, improving traceability and error handling.
    - Updated environment configuration to require LOG_API_URL for logging service integration.

--- game-backend/player-service/src/config/env.ts
+++ game-backend/player-service/src/config/env.ts
@@ -4,10 +4,13 @@ dotenv.config();
 if (!process.env.MONGO_URI) throw new Error('❌ MONGO_URI is missing');
 if (!process.env.KAFKA_BROKER) throw new Error('❌ KAFKA_BROKER is missing');
 if (!process.env.KAFKA_RETRY_TOPIC) throw new Error('❌ KAFKA_RETRY_TOPIC is missing');
-
+if (!process.env.LOG_API_URL) {
+  throw new Error('❌ LOG_API_URL is not defined');
+}
 export const env = {
   port: process.env.PORT || 3001,
   mongoUri: process.env.MONGO_URI,
   kafkaBroker: process.env.KAFKA_BROKER,
   kafkaRetryTopic: process.env.KAFKA_RETRY_TOPIC,
+  logApiUrl: process.env.LOG_API_URL,
 };
